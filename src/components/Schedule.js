af3a45084005730fc4d84a5e7b1ba34c18a1663f
Matthew Cheah <matthew.cheah@ibm.com>
2019-06-12 12:07:56 +1000

    Update schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Heading from './Heading'
-
+import Heading from "./Heading";
 
 export default class Schedule extends Component {
   render() {
     return (
-      <div style={{ backgroundColor: "#404040", height: "100%", padding: "25px" }}>
-        <div style={{marginLeft: "auto", marginRight: "auto", maxWidth: "1200px"}}>
+      <div
+        style={{
+          backgroundColor: "#404040",
+          height: "100%",
+          padding: "25px"
+        }}
+      >
+        <div
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: "1200px"
+          }}
+        >
+          <div style={{ color: "white" }}>
+            <Heading title="SCHEDULE" />
+          </div>
 
-
-        <div style={{color: "white"}}>
-          <Heading title="SCHEDULE"/>
+          <img
+            style={{
+              width: "100%",
+              display: "block",
+              margin: "auto",
+              paddingBottom: "30px"
+            }}
+            src={require("../images/updated_schedule.png")}
+          />
         </div>
-
-        <img 
-                style={{width:"100%", display: "block", margin:"auto", paddingBottom:"30px"}}
-                src={require("../images/darksched.png")}
-              />
-        
       </div>
-      </div>
-    )
+    );
   }
 }
